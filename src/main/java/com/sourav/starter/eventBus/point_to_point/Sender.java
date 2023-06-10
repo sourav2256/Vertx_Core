4b9acd3cb724f282c22c927811909bc8ba97c289
sourav2256 <souravpal7217@gmail.com>
2023-06-10 13:05:46 +0530

    point to point event bus

---
+++ src/main/java/com/sourav/starter/eventBus/point_to_point/Sender.java
@@ -0,0 +1,18 @@
+package com.sourav.starter.eventBus.point_to_point;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+import io.vertx.core.impl.logging.Logger;
+import io.vertx.core.impl.logging.LoggerFactory;
+
+public class Sender extends AbstractVerticle {
+  private static final Logger LOG = LoggerFactory.getLogger(Sender.class);
+
+  @Override
+  public void start(Promise<Void> startPromise) throws Exception {
+    startPromise.complete();
+    vertx.setPeriodic(1000, event -> {
+      vertx.eventBus().send(Sender.class.getName(), "Sending a message......");
+    });
+  }
+}
